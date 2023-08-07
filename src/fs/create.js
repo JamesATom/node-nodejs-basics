b7cb246fa18b77f92809cf45a223ac3e18c80c73
Abdulaziz-Abduhakhimov <abduhakimovabdulaziz7@gmail.com>
2023-08-07 10:41:40 +0500

    feat: implement create method in src/fs/create.js folder

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import * as fs from 'node:fs';
+import path from 'node:path';
+import url from 'node:url';
+import { writeFile } from 'node:fs/promises';
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+const filename = path.join(__dirname, 'files', 'fresh.txt');
+const data = 'I am fresh and young';
+
 const create = async () => {
-    // Write your code here 
+    try {
+        await writeFile(filename, data, { flag: 'wx+' });
+    } catch(err) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await create();
