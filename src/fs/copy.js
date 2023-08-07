fcf04555c63c3a071e46723ac5c15673ee700b1d
Abdulaziz-Abduhakhimov <abduhakimovabdulaziz7@gmail.com>
2023-08-07 09:19:50 +0500

    Revert "update changes"
    This reverts commit a4df3849a5e7cedb4c23b90f4bc8bff21080ee82.

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,12 +1,5 @@
-const fs = require("node:fs");
-
 const copy = async () => {
-    console.log('hello world');
+    // Write your code here 
 };
 
-copy();
-
-
-
-// copy.js - implement function that copies folder files files with all its content into folder files_copy at the same level 
-// (if files folder doesn't exists or files_copy has already been created Error with message FS operation failed must be thrown)
+await copy();
