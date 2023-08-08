5682cf2a5b80cb9e4dd88e13014c7f00e84985c1
Abdulaziz-Abduhakhimov <abduhakimovabdulaziz7@gmail.com>
2023-08-08 23:15:04 +0500

    feat: implement the copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,4 @@
+import fsPromises from 'node:fs/promises';
 import fs from 'node:fs';
 import { fileToUrl } from "../utils/utils.js";
 
@@ -5,6 +6,23 @@ const dirName = fileToUrl(import.meta.url);
 
 const copy = async () => {
     console.log('hello world: ', dirName);
+    try {
+        await fsPromises.access(dirName + '/files', fs.constants.R_OK);
+        fs.access(dirName + '/files_copy', (err) => {
+            if (err) {
+                fs.cp(dirName + '/files', dirName + '/files_copy', { recursive: true }, (err) => {
+                    if (!err) {
+                        throw new Error('FS operation failed')
+                    }
+                });
+            } else {
+                throw new Error('FS operation failed')
+            }
+        });
+        console.log('FS operation succeeded');
+    } catch(err) {
+        throw new Error('FS operation failed');
+    }
 };
 
 copy();
