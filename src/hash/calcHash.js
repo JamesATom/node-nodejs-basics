09008c96107cfefb6360bc257b99053f7129b59e
Abdulaziz-Abduhakhimov <abduhakimovabdulaziz7@gmail.com>
2023-08-09 12:40:54 +0500

    feat: implement method called calculateHash in src/hash/calcHash.js folder

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import crypto from 'node:crypto';
+import fsPromises from 'node:fs/promises';
+import process from 'node:process';
+import url from 'node:url';
+
 const calculateHash = async () => {
-    // Write your code here 
+    try {
+        const filePath = new url.URL('./files/fileToCalculateHashFor.txt', import.meta.url);
+        const content = await fsPromises.readFile(filePath, { encoding: 'utf-8' });
+        const value = crypto.createHash('sha256').update(content, 'utf-8').digest('hex');
+        process.stdout.write(value);
+    } catch {
+        throw new Error('FS operation failed');
+    }
 };
 
 await calculateHash();
