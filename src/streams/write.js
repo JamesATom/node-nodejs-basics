541312fa7be6b16b7df5004b9ba163abb1dcde9c
Abdulaziz-Abduhakhimov <abduhakimovabdulaziz7@gmail.com>
2023-08-10 13:05:01 +0500

    feat: implement write.js with process.stdin.on

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,19 @@
+import process from 'node:process';
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileToUrl } from '../utils/utils.js';
+
+const dirName = fileToUrl(import.meta.url);
+const desFile = path.join(dirName, 'files', 'fileToWrite.txt');
+
 const write = async () => {
-    // Write your code here 
+    const writerStream = fs.createWriteStream(desFile, { encoding: 'utf-8' });
+    process.stdin.on('readable', () => {
+        let chunk;
+        while ((chunk = process.stdin.read()) !== null) {
+            writerStream.write(chunk);
+        }
+    });
 };
 
 await write();
