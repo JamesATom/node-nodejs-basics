d50644b2aa696d7365b4e2ef53738a4cdf300a4c
Abdulaziz-Abduhakhimov <abduhakimovabdulaziz7@gmail.com>
2023-08-10 12:28:38 +0500

    feat: implement different way of reading data using Stream API of node.js docs on git branch called node-basics2

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,19 @@
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileToUrl } from '../utils/utils.js';
+
+const dirName = fileToUrl(import.meta.url);
+const srcFile = path.join(dirName, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+    const logChunksOfFile = async (chunks) => {
+        for await (let chunk of chunks) {
+            console.log(chunk);
+        }
+    } 
+
+    const chunkContent = fs.createReadStream(srcFile, { encoding: 'utf-8' });
+    logChunksOfFile(chunkContent);
 };
 
 await read();
