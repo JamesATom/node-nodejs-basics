6fac2236552e68f5e3033b37a0558b6ced565c02
Abdulaziz-Abduhakhimov <abduhakimovabdulaziz7@gmail.com>
2023-08-10 12:15:16 +0500

    feat: implement the createReadStream from file.txt

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,14 @@
+import process from 'node:process';
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileToUrl } from '../utils/utils.js';
+
+const dirName = fileToUrl(import.meta.url);
+const srcFile = path.join(dirName, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+    const content = fs.createReadStream(srcFile, { encoding: 'utf-8' });
+    content.pipe(process.stdout);
 };
 
 await read();
