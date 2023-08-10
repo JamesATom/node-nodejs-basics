ad87870bacef35efaab3d9ebbdd6f6cc9ab89720
Abdulaziz-Abduhakhimov <abduhakimovabdulaziz7@gmail.com>
2023-08-10 14:14:47 +0500

    feat: implement src/streams/transform.js method called transform in node-basics branch

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,19 @@
+import stream from 'node:stream';
+
 const transform = async () => {
-    // Write your code here 
+    const reversedStr = new stream.Transform({
+        transform(chunk, encoding, callback) {
+            callback(null, chunk.toString().split("").reverse().join("") + '\n');
+        }
+    });
+    
+    stream.pipeline(process.stdin, reversedStr, process.stdout,  (err) => {
+        if (err) {
+          console.error("An error occured in pipeline.", err);
+        } else {
+          console.log("Pipeline execcution successful");
+        }
+    });
 };
 
 await transform();
