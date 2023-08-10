0dcf814d8466737312e1aefd7225486b60e1b98a
Abdulaziz-Abduhakhimov <abduhakimovabdulaziz7@gmail.com>
2023-08-10 13:08:48 +0500

    feat: implement write.js in branch called node-basics2 with different method than branch called node-basics

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,15 @@
+import process from 'node:process';
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileToUrl } from '../utils/utils.js';
+
+const dirName = fileToUrl(import.meta.url);
+const desFile = path.join(dirName, 'files', 'fileToWrite.txt');
+
 const write = async () => {
-    // Write your code here 
+    const writerStream = fs.createWriteStream(desFile, { encoding: 'utf-8' });
+    // process.stdin.on('data', chunk => writerStream.write(chunk));
+    process.stdin.pipe(writerStream);
 };
 
 await write();
