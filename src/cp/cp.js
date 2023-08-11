e18ab1cffebfc91517ad4cf699fdc1317dd4e641
Abdulaziz-Abduhakhimov <abduhakimovabdulaziz7@gmail.com>
2023-08-11 06:02:25 +0500

    feat: implement src/cp/cp.js and /files/script.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,13 @@
+import path from 'node:path';
+import cp from 'node:child_process';
+import { fileToUrl } from '../utils/utils.js';
+
+const dirName = fileToUrl(import.meta.url);
+const childModule = path.join(dirName, 'files', 'script.js');
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    cp.fork(childModule, args);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess([1, 2, 3]);
